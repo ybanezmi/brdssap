36923e3601db61f2d8b88e6bdae109e398141bb7
Michelle Ybanez <ybanez.mich@gmail.com>
2015-07-17 10:50:11 +0800

    [Receiving] Replaced callBAPIFunction with callBAPIReceiving.

--- app/Controller/Component/SapRfcComponent.php
+++ app/Controller/Component/SapRfcComponent.php
@@ -95,42 +95,65 @@ class SapRfcComponent extends Component {
         return $response;
 	}
 
-    public function callBAPIFunction($rfcfunction, $params = null) {
+    public function callBAPIReceiving($params = null) {
         $rfc = $this->open();
-		$response = array();
 
         if (isset($rfc['error'])) {
             return $rfc;
         }
 
-		if (!function_exists('saprfc_function_discover')) {
-            $response['error'] = Configure::read('SAP.ERROR.100');
-            return $response;
-        }
-		
         // Locate the function and discover the interface
-		$rfchandle = saprfc_function_discover($rfc, $rfcfunction);
+		$rfchandle = saprfc_function_discover($rfc, Configure::read('SAP.ZBAPI_RECEIVING.FUNCTION_NAME'));
 
 		if (!$rfchandle) {
-		    $rfcerror = str_replace('{0}', $rfcfunction, Configure::read('SAP.ERROR.102'));
+		    $rfcerror = str_replace('{0}', Configure::read('SAP.ZBAPI_RECEIVING.FUNCTION_NAME'), Configure::read('SAP.ERROR.102'));
             $response['error'] = str_replace('{1}', saprfc_error($rfc), $rfcerror);
             return $response;
 		}
 
-        // Import by rfc function
-        switch($rfcfunction) {
-            case Configure::read('SAP.ZBAPI_RECEIVING.FUNCTION_NAME'):
-                $response = $this->handleBAPIReceiving($rfchandle, $rfc_rc, $params, $response);
-                break;
-            case Configure::read('SAP.ZBAPI_CTO_PPTAG.FUNCTION_NAME'):
-                $response = $this->handleBAPICT0PPTag($rfchandle, $rfc_rc, $params, $response);
-                break;
-            case Configure::read('SAP.ZBAPI_POST_GR.FUNCTION_NAME'):
-                $response = $this->handleBAPIPostGR($rfchandle, $rfc_rc, $params, $response);
-                break;
-            default:
-                break;
-        }
+        saprfc_import($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.RS_INPUT'), $params);
+        saprfc_import($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.PRINTER'), Configure::read('SAP.ZBAPI_RECEIVING.PRINTER_VAL'));
+
+		saprfc_table_init($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.ET_PALLETS'));
+		saprfc_table_init($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.ET_PALLETS_W_TO'));
+
+        $rfc_rc = saprfc_call_and_receive($rfchandle);
+        $sn = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.VBELN'));
+		$ol = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.OBJECT_LOCKED'));
+		$nc = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.NOT_COMPATIBLE'));
+        $wc = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.WEIGHT_CAP_ERROR'));
+		$vc = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.VOLUME_CAP_ERROR'));
+		$oe = saprfc_export($rfchandle, Configure::read('SAP.ZBAPI_RECEIVING.OTHER_ERROR'));
+
+        $data_et_pallets = saprfc_table_rows($rfchandle, 'SAP.ZBAPI_RECEIVING.ET_PALLETS');
+		$data_et_pallets_w_to = saprfc_table_rows($rfchandle, 'SAP.ZBAPI_RECEIVING.ET_PALLETS_W_TO');
+
+        if ($rfc_rc != SAPRFC_OK) {
+            if ($rfc_rc == SAPRFC_EXCEPTION) {
+                $response['error'] = str_replace('{0}', saprfc_exception($rfchandle), Configure::read('SAP.ERROR.103'));
+            } else {
+                $response['error'] = str_replace('{0}', saprfc_error(), Configure::read('SAP.ERROR.104'));
+            }
+        } else {
+            // SAP inbound no successfully retrieved
+            if ($sn <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['sap_inbound_no'] = $sn;
+                $response['pallet_no'] = $data_et_pallets;
+            // SAP bapi function error occurred
+            } else if ($ol <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['error'] = Configure::read('SAP.ERROR.300');
+            } else if ($nc <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['error'] = Configure::read('SAP.ERROR.301');
+            } else if ($wc <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['error'] = Configure::read('SAP.ERROR.302');
+            } else if ($vc <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['error'] = Configure::read('SAP.ERROR.303');
+            } else if ($oe <> Configure::read('CONST.EMPTY_STRING')) {
+                $response['error'] = Configure::read('SAP.ERROR.304');
+            } else {
+                // Do nothing
+            }
+         }
 
         $this->close($rfchandle, $rfc);
 
